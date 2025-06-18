c2658a0a3b6c1610fd85432c65181bac0e1c9d01
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2025-06-17 22:15:45 -0700

    disable migration for the future

--- db/migrations/20241129224536_add_explicit.sql
+++ db/migrations/20241129224536_add_explicit.sql
@@ -1,11 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
 SELECT 'up SQL query';
-ALTER TABLE media_file
-    ADD COLUMN explicit bool default false;
+-- Comment out as these aren't used anymore
+-- ALTER TABLE media_file
+    -- ADD COLUMN explicit bool default false;
 
-ALTER TABLE album
-    ADD COLUMN explicit bool default false;
+-- ALTER TABLE album
+    -- ADD COLUMN explicit bool default false;
 -- +goose StatementEnd
 
 -- +goose Down
